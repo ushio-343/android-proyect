eff3709d69e2df11f84d19eb863e9c214f74e37a
Rogelio E <100113445+ushio-343@users.noreply.github.com>
2024-06-19 12:31:29 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/ejem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ejem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset('assets/ilustracion1.png'),
+          Text('!HOLA! Organiza tus dias alcanza tus metas',
+          textAlign: TextAlign.center,
+          ),
+          Icon(Icons.arrow_forward,
+          color: Colors.yellow[700],
+          
+          )
+        
+        ],
+      )
+    );
+  }
+}
